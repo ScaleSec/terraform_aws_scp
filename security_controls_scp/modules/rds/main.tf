aa334ab73cc2c1a4022c6ca58090126de70dcad5
jdyke <jasonadyke@gmail.com>
2020-07-23 15:03:31 -0400

    rds_unencrypted updates

---
+++ security_controls_scp/modules/rds/main.tf
@@ -0,0 +1,43 @@
+#-----security_controls_scp/modules/rds/main.tf----#
+
+data "aws_iam_policy_document" "deny_unencrypted_rds_actions" {
+  statement {
+    sid = "DenyUnencryptedRds"
+
+    actions = [
+      "rds:CreateDBCluster",
+      "rds:CreateDBInstance",
+      "rds:RestoreDBClusterFromS3",
+      "rds:RestoreDBInstanceFromS3",
+      "rds:RestoreDBClusterFromDBSnapshot",
+      "rds:RestoreDBClusterToPointInTime",
+    ]
+
+    resources = [
+      "*",
+    ]
+
+    effect = "Deny"
+
+    condition {
+      test     = "Bool"
+      variable = "rds:StorageEncrypted"
+
+      values = [
+        "false",
+      ]
+    }
+  }
+}
+
+resource "aws_organizations_policy" "deny_unencrypted_rds_actions" {
+  name        = "Deny Unencrypted RDS Actions"
+  description = "Deny any unencrypted RDS action that supports an encryption parameter."
+
+  content = data.aws_iam_policy_document.deny_unencrypted_rds_actions.json
+}
+
+resource "aws_organizations_policy_attachment" "deny_unencrypted_rds_actions_attachment" {
+  policy_id = aws_organizations_policy.deny_unencrypted_rds_actions.id
+  target_id = var.target_id
+}

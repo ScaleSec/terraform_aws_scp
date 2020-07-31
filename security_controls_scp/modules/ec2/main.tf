f4202149428884bd3a8855245fbfc03ec660e147
Jason Dyke <19151605+jdyke@users.noreply.github.com>
2020-07-31 14:12:49 -0400

    ec2 public ami deny (#47)

--- security_controls_scp/modules/ec2/main.tf
+++ security_controls_scp/modules/ec2/main.tf
@@ -120,3 +120,43 @@ resource "aws_organizations_policy_attachment" "require_ami_tag_attachment" {
   policy_id = aws_organizations_policy.require_ami_tag_policy.id
   target_id = var.target_id
 }
+
+
+## Denies Users from launching EC2s from Public AMIs
+
+data "aws_iam_policy_document" "deny_ec2_public_ami" {
+  statement {
+    sid = "DenyEc2PublicAMI"
+
+    actions = [
+      "ec2:RunInstances",
+    ]
+
+    resources = [
+      "arn:aws:ec2:*::image/*",
+    ]
+
+    effect = "Deny"
+
+    condition {
+      test     = "Bool"
+      variable = "ec2:Public"
+
+      values = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_organizations_policy" "deny_ec2_public_ami" {
+  name        = "Deny EC2 Public AMI"
+  description = "Denies users the ability to launch EC2 instances with public AMIs."
+
+  content = data.aws_iam_policy_document.deny_ec2_public_ami.json
+}
+
+resource "aws_organizations_policy_attachment" "deny_ec2_public_ami_attachment" {
+  policy_id = aws_organizations_policy.deny_ec2_public_ami.id
+  target_id = var.target_id
+}

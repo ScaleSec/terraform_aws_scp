8d9d07aa2d2d839e96eface41c798096c2329c64
jdyke <jasonadyke@gmail.com>
2021-04-05 14:05:34 -0400

    terraform fmt updates

--- security_controls_scp/modules/comprehend/require_kms_cmks.tf
+++ security_controls_scp/modules/comprehend/require_kms_cmks.tf
@@ -1,17 +1,17 @@
 #-----security_controls_scp/modules/sagemaker/require_kms_cmks.tf----#
 
 data "template_file" "require_kms_cmks_policy" {
-    template = file("./templates/require_kms_cmks.json")
+  template = file("./templates/require_kms_cmks.json")
 }
 
 resource "aws_organizations_policy" "require_kms_cmks_policy" {
-    name        = "Comprehend require KMS CMKs"
-    description = "Requires all applicable Comprehend APIs use KMS CMKs."
+  name        = "Comprehend require KMS CMKs"
+  description = "Requires all applicable Comprehend APIs use KMS CMKs."
 
-    content = data.template_file.require_kms_cmks_policy.rendered
+  content = data.template_file.require_kms_cmks_policy.rendered
 }
 
 resource "aws_organizations_policy_attachment" "require_kms_cmks_attachment" {
-    policy_id = aws_organizations_policy.require_kms_cmks_policy.id
-    target_id = var.target_id
+  policy_id = aws_organizations_policy.require_kms_cmks_policy.id
+  target_id = var.target_id
 }
--- security_controls_scp/modules/comprehend/require_private_vpcs.tf
+++ security_controls_scp/modules/comprehend/require_private_vpcs.tf
@@ -1,17 +1,17 @@
 #-----security_controls_scp/modules/sagemaker/require_private_vpc.tf----#
 
 data "template_file" "comprehend_require_private_vpc_policy" {
-    template = file("./templates/require_private_vpcs.json")
+  template = file("./templates/require_private_vpcs.json")
 }
 
 resource "aws_organizations_policy" "comprehend_require_private_vpc_policy" {
-    name        = "Require private VPCs Amazon Comprehend"
-    description = "Requires that all Amazon Comprehend jobs use a private VPC."
+  name        = "Require private VPCs Amazon Comprehend"
+  description = "Requires that all Amazon Comprehend jobs use a private VPC."
 
-    content = data.template_file.comprehend_require_private_vpc_policy.rendered
+  content = data.template_file.comprehend_require_private_vpc_policy.rendered
 }
 
 resource "aws_organizations_policy_attachment" "comprehend_require_private_vpc_attachment" {
-    policy_id = aws_organizations_policy.comprehend_require_private_vpc_policy.id
-    target_id = var.target_id
+  policy_id = aws_organizations_policy.comprehend_require_private_vpc_policy.id
+  target_id = var.target_id
 }

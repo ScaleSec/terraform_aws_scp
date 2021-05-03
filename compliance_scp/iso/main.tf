469288acb110a96b72e4b986eea8f45f09bf7ab6
jdyke <jasonadyke@gmail.com>
2021-05-03 10:11:08 -0400

    iso

--- compliance_scp/iso/main.tf
+++ compliance_scp/iso/main.tf
@@ -1,14 +1,18 @@
 # The below approved services are based off the list located here: https://aws.amazon.com/compliance/services-in-scope/
 
-data "template_file" "iso_policy" {
-  template = file("../templates/iso.json")
+data "http" "iso_policy" {
+  url = "https://raw.githubusercontent.com/salesforce/aws-allowlister/main/examples/latest/ISO-AllowList-SCP.json"
+
+  request_headers = {
+    Accept = "application/json"
+  }
 }
 
 resource "aws_organizations_policy" "allow_iso_services_policy" {
   name        = "Allow ISO Services"
-  description = "Only allow ISO services as of 03/2021"
+  description = "Only allow ISO services."
 
-  content = data.template_file.iso_policy.rendered
+  content = data.http.iso_policy.body
 }
 
 resource "aws_organizations_policy_attachment" "allow_iso_services_attachment" {

f8259e913301b094219e80e7507ae367d9200671
jdyke <jasonadyke@gmail.com>
2021-05-03 10:14:11 -0400

    soc

--- compliance_scp/soc/main.tf
+++ compliance_scp/soc/main.tf
@@ -1,14 +1,18 @@
 # The below approved services are based off the list located here: https://aws.amazon.com/compliance/services-in-scope/
 
-data "template_file" "soc_policy" {
-  template = file("../templates/soc.json")
+data "http" "soc_policy" {
+  url = "https://raw.githubusercontent.com/salesforce/aws-allowlister/main/examples/latest/SOC-AllowList-SCP.json"
+
+  request_headers = {
+    Accept = "application/json"
+  }
 }
 
 resource "aws_organizations_policy" "allow_soc_services_policy" {
   name        = "Allow SOC Services"
-  description = "Only allow SOC services as of 02/2021"
+  description = "Only allow SOC services."
 
-  content = data.template_file.soc_policy.rendered
+  content = data.http.soc_policy.body
 }
 
 resource "aws_organizations_policy_attachment" "allow_soc_services_attachment" {
